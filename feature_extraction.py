431957b49a028a72ceb7e3a7452836755d477f7c
Luuk Romeijn <luukromeijn@hotmail.nl>
2024-05-14 09:05:42 +0200

    Improved feature extraction script

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,24 +1,24 @@
 import argparse
+import numpy as np
 from rhythmnblues.features import *
 from rhythmnblues.data import Data
 from rhythmnblues import utils
 
 utils.watch_progress(on=False) # For SHARK
 
-def feature_extraction(pcrna_filepath, ncrna_filepath, export_table_filepath, 
-                       export_features_folder, 
-                       blastdbs_path='data/blastdbs/uniref90',
-                       fickett_ref='data/features/fickett_paper.txt',
-                       tmp_folder='', 
-                       test_mode=False):
-
+def feature_extraction(
+        dataset_name, pcrna_filepath, ncrna_filepath, tables_folder, 
+        features_folder, blastdbs_path='data/blastdbs/uniref90', tmp_folder='', 
+        test_mode=False
+):
     data = Data([pcrna_filepath, ncrna_filepath])
+    data.calculate_feature(Length())
+    data.filter_outliers('length', [100, np.inf])
     if test_mode:
-        data = data.sample(100, 100) # TODO uncomment
+        data = data.sample(100, 100)
 
     features = [
-        lambda data: Length(),
-        lambda data: FickettScore(fickett_ref),
+        lambda data: FickettScore(f'{features_folder}/fickett_paper.txt'),
         lambda data: Complexity(),
         lambda data: Quality(),
         lambda data: ORFCoordinates(relaxation=0),
@@ -61,41 +61,42 @@ def feature_extraction(pcrna_filepath, ncrna_filepath, export_table_filepath,
         lambda data: Entropy('3-mer entropy', KmerFreqs(k=3, apply_to='sequence', stride=1).name),
         lambda data: Entropy('3-mer ORF entropy', KmerFreqs(k=3, apply_to='ORF', stride=3).name),
         lambda data: EntropyDensityProfile(KmerFreqs(k=2, apply_to='ORF', stride=1).name), 
-        lambda data: ZhangScore(data, export_path=f'{export_features_folder}/zhang_ref.txt'), 
-        lambda data: KmerScore(data, k=6, apply_to='sequence', export_path=f'{export_features_folder}/6mer_ref.txt'),
-        lambda data: MLCDS(data, export_path=f'{export_features_folder}/mlcds_ref.txt'), 
+        lambda data: ZhangScore(data, export_path=f'{features_folder}/{dataset_name}/zhang_ref.txt'), 
+        lambda data: KmerScore(data, k=6, apply_to='sequence', export_path=f'{features_folder}/{dataset_name}/6mer_ref.txt'),
+        lambda data: MLCDS(data, export_path=f'{features_folder}/{dataset_name}/mlcds_ref.txt'), 
         lambda data: MLCDSLength(),
         lambda data: MLCDSLengthPercentage(),
         lambda data: MLCDSLengthStd(),
         lambda data: MLCDSScoreDistance(),
         lambda data: MLCDSScoreStd(),
-        lambda data: BLASTXSearch(blastdbs_path, tmp_folder=tmp_folder, threads=12),
+        lambda data: BLASTXSearch(blastdbs_path, tmp_folder=f'{tmp_folder}/{dataset_name}', threads=12),
         lambda data: BLASTXBinary(),
         lambda data: SSE(),
         lambda data: UPFrequency(),
-        lambda data: KmerDistance(data, 6, 'log', 'ORF', stride=3, export_path=f'{export_features_folder}/6mer_orf_dist_ref.txt'),
-        lambda data: KmerDistance(data, 4, 'log', 'acguD', stride=1, alphabet='ACGTD', export_path=f'{export_features_folder}/4mer_acgtd_dist_ref.txt'), 
-        lambda data: KmerDistance(data, 3, 'log', 'acgu-ACGU', stride=1, alphabet='ACGTacgt', export_path=f'{export_features_folder}/3mer_acgu_dist_ref.txt'),
+        lambda data: KmerDistance(data, 6, 'log', 'ORF', stride=3, export_path=f'{features_folder}/{dataset_name}/6mer_orf_dist_ref.txt'),
+        lambda data: KmerDistance(data, 4, 'log', 'acguD', stride=1, alphabet='ACGTD', export_path=f'{features_folder}/{dataset_name}/4mer_acgtd_dist_ref.txt'), 
+        lambda data: KmerDistance(data, 3, 'log', 'acgu-ACGU', stride=1, alphabet='ACGTacgt', export_path=f'{features_folder}/{dataset_name}/3mer_acgu_dist_ref.txt'),
     ]
 
     for feature in features:
         feature = feature(data)
         data.calculate_feature(feature)
-        data.to_hdf(export_table_filepath)
+        data.to_hdf(f'{tables_folder}/{dataset_name}.h5')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('dataset_name')
     parser.add_argument('pcrna_filepath')
     parser.add_argument('ncrna_filepath')
-    parser.add_argument('export_table_filepath')
-    parser.add_argument('export_features_folder')
+    parser.add_argument('tables_folder')
+    parser.add_argument('features_folder')
     parser.add_argument('--blastdbs_path', default='data/blastdbs/uniref90')
-    parser.add_argument('--fickett_ref',default='data/features/fickett_paper.txt')
     parser.add_argument('--tmp_folder', default='')
     parser.add_argument('--test_mode', type=bool, default=False)
     args = parser.parse_args()
-    feature_extraction(args.pcrna_filepath, args.ncrna_filepath, 
-                       args.export_table_filepath, args.export_features_folder, 
-                       args.blastdbs_path, args.fickett_ref, args.tmp_folder, 
-                       args.test_mode)
+    feature_extraction(
+        args.dataset_name, args.pcrna_filepath, args.ncrna_filepath, 
+        args.tables_folder, args.features_folder, args.blastdbs_path, 
+        args.tmp_folder, args.test_mode
+    )

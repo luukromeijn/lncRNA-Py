7bb5c4a205e99379329ac53ce70fef30dfa72276
Luuk Romeijn <luukromeijn@hotmail.nl>
2024-06-11 12:23:06 +0200

    Improved unittests for new wrapper design and mlm task

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,9 +1,9 @@
 import pytest
 import torch
-from rhythmnblues.features import KmerFreqs
-from rhythmnblues.modules import MycoAICNN, Model
+from rhythmnblues.features import KmerFreqs, KmerTokenizer
+from rhythmnblues.modules import MycoAICNN, Classifier, BERT, MLM
 from rhythmnblues.train.loggers import *
-from rhythmnblues.train import train_classifier
+from rhythmnblues.train import train_classifier, train_mlm
 from rhythmnblues import utils
 
 loggers = [
@@ -18,8 +18,8 @@ def logger(request, tmp_path):
     _, fixture_func = request.param
     return fixture_func(tmp_path)
 
-def test_logger_set_columns(logger):
-    logger.set_columns({'Test1': lambda x: x})
+def test_logger_start(logger):
+    logger.start({'Test1': lambda x: x})
     assert len(logger.columns) == 4
     assert logger.columns[0] == 'Loss|train'
     assert logger.columns[1] == 'Test1|train'
@@ -27,15 +27,22 @@ def test_logger_set_columns(logger):
     assert logger.columns[3] == 'Test1|valid'
 
 def test_logger_log(logger):
-    logger.set_columns({})
+    logger.start({})
     logger.log([0,0])
 
 def test_train_classifier(data):
     kmers = KmerFreqs(3)
-    model = Model(MycoAICNN())
+    model = Classifier(MycoAICNN())
     data.calculate_feature(kmers)
     data.set_tensor_features(kmers.name)
     train_classifier(model, data, data, 1)
+
+def test_train_mlm(data):
+    kmers = KmerTokenizer(3)
+    model = MLM(BERT(kmers.vocab_size, d_model=16, d_ff=32))
+    data.calculate_feature(kmers)
+    data.set_tensor_features(kmers.name, torch.long)
+    train_mlm(model, data, data, 1)
 
 def test_utils_device():
     '''The code assumes that device is device object (and not a string!)'''
--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -1,14 +1,48 @@
 import pytest
-from rhythmnblues.modules import Classifier
-from rhythmnblues.modules import MycoAICNN
-from rhythmnblues.features import KmerFreqs
+import torch
+from rhythmnblues.modules import Classifier, MLM
+from rhythmnblues.modules.wrappers import WrapperBase
+from rhythmnblues.modules import MycoAICNN, BERT
+from rhythmnblues.features import KmerFreqs, KmerTokenizer
 
 
-def test_model(data):
-    kmers = KmerFreqs(3)
-    model = Classifier(MycoAICNN())
-    data.calculate_feature(kmers)
-    data.set_tensor_features(kmers.name)
+def test_wrapper_base(data):
+    encoder = KmerTokenizer(3)
+    data.calculate_feature(encoder)
+    data.set_tensor_features(encoder.name, torch.long)
+    model = WrapperBase(BERT(encoder.vocab_size))
     model.predict(data)
+    model.latent_space(data)
 
-# TODO more unittests required to ensure proper behaviour of WrapperBase
+def test_classifier_cnn(data):
+    encoder = KmerFreqs(3)
+    data.calculate_feature(encoder)
+    data.set_tensor_features(encoder.name)
+    model = WrapperBase(MycoAICNN())
+    model.predict(data)
+    model.latent_space(data, CLS_only=False)
+
+def test_classifier_bert(data):
+    encoder = KmerTokenizer(3)
+    data.calculate_feature(encoder)
+    data.set_tensor_features(encoder.name, torch.long)
+    model = Classifier(BERT(encoder.vocab_size))
+    model.predict(data)
+    model.latent_space(data)
+
+def test_classifier_return_logits(data):
+    encoder = KmerTokenizer(3)
+    data.calculate_feature(encoder)
+    data.set_tensor_features(encoder.name, torch.long)
+    model = Classifier(BERT(encoder.vocab_size))
+    pred_probs = model.predict(data, return_logits=False)
+    pred_logits = model.predict(data, return_logits=True)
+    assert (torch.sigmoid(pred_logits) == pred_probs).sum() == len(data)
+
+def test_mlm(data):
+    encoder = KmerTokenizer(3)
+    data.calculate_feature(encoder)
+    data.set_tensor_features(encoder.name, torch.long)
+    model = MLM(BERT(encoder.vocab_size))
+    model.predict(data)
+    model.latent_space(data)

01b305fdf0236dd6b60d87f418fc15e6c55d9563
Luuk Romeijn <luukromeijn@hotmail.nl>
2024-09-09 04:11:14 -0400

    Safer device management for `NUC_TO_4D` (still problematic on vibranium tho)

--- rhythmnblues/utils.py
+++ rhythmnblues/utils.py
@@ -26,20 +26,20 @@ TOKENS = {'MASK': 0, 'CLS': 1, 'SEP': 2, 'PAD': 3, 'UNK': 4}
 BPE to assume that MASK=0, hence this value should not be changed.'''
 
 NUC_TO_4D = {
-    'A': torch.tensor([1,    0,    0,    0   ], device=DEVICE),
-    'C': torch.tensor([0,    1,    0,    0   ], device=DEVICE),
-    'G': torch.tensor([0,    0,    1,    0   ], device=DEVICE),
-    'T': torch.tensor([0,    0,    0,    1   ], device=DEVICE),
-    'R': torch.tensor([0.5,  0,    0.5,  0   ], device=DEVICE),
-    'Y': torch.tensor([0,    0.5,  0,    0.5 ], device=DEVICE),
-    'S': torch.tensor([0,    0.5,  0.5,  0   ], device=DEVICE),
-    'W': torch.tensor([0.5,  0,    0,    0.5 ], device=DEVICE),
-    'K': torch.tensor([0,    0,    0.5,  0.5 ], device=DEVICE),
-    'M': torch.tensor([0.5,  0.5,  0,    0   ], device=DEVICE),
-    'B': torch.tensor([0,    1/3,  1/3,  1/3 ], device=DEVICE),
-    'D': torch.tensor([1/3,  0,    1/3,  1/3 ], device=DEVICE),
-    'H': torch.tensor([1/3,  1/3,  0,    1/3 ], device=DEVICE),
-    'V': torch.tensor([1/3,  1/3,  1/3,  0   ], device=DEVICE),
+    'A': torch.tensor([1.0,  0.0,  0.0,  0.0 ], device=DEVICE),
+    'C': torch.tensor([0.0,  1.0,  0.0,  0.0 ], device=DEVICE),
+    'G': torch.tensor([0.0,  0.0,  1.0,  0.0 ], device=DEVICE),
+    'T': torch.tensor([0.0,  0.0,  0.0,  1.0 ], device=DEVICE),
+    'R': torch.tensor([0.5,  0.0,  0.5,  0.0 ], device=DEVICE),
+    'Y': torch.tensor([0.0,  0.5,  0.0,  0.5 ], device=DEVICE),
+    'S': torch.tensor([0.0,  0.5,  0.5,  0.0 ], device=DEVICE),
+    'W': torch.tensor([0.5,  0.0,  0.0,  0.5 ], device=DEVICE),
+    'K': torch.tensor([0.0,  0.0,  0.5,  0.5 ], device=DEVICE),
+    'M': torch.tensor([0.5,  0.5,  0.0,  0.0 ], device=DEVICE),
+    'B': torch.tensor([0.0,  1/3,  1/3,  1/3 ], device=DEVICE),
+    'D': torch.tensor([1/3,  0.0,  1/3,  1/3 ], device=DEVICE),
+    'H': torch.tensor([1/3,  1/3,  0.0,  1/3 ], device=DEVICE),
+    'V': torch.tensor([1/3,  1/3,  1/3,  0.0 ], device=DEVICE),
     'N': torch.tensor([0.25, 0.25, 0.25, 0.25], device=DEVICE),
 }
 '''Mapping from nucleotide indicator to 4D_DNA representation, which is the 
@@ -50,6 +50,14 @@ LEN_4D_DNA = 8000
 sequences are zero-padded.'''
 
 ##### HELPER FUNCTIONS #####
+def change_device(device): # NOTE: not sure if this is neccessary
+    '''Safely change PyTorch device, also moves certain constant variables 
+    contained within `rhythmnblues.utils` to the specified device.'''
+    global NUC_TO_4D, DEVICE
+    DEVICE = torch.device(device)
+    for nuc in NUC_TO_4D:
+        NUC_TO_4D[nuc] = NUC_TO_4D[nuc].to(DEVICE)
+
 def watch_progress(on=True):
     '''Switches on/off the `tqdm` progress indicator around large for loops.'''
     global progress
